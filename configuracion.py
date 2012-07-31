fec726c9f4a72b058bc040abadffbb7195f536c4
mmartinovic <marcelo.martinovic@gmail.com>
2012-07-31 12:43:23 -0300

    act

---
+++ configuracion.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 *-*
+
+
+dirNoListables = ['templates_c',
+    'accionesMasivas',
+    'accionesMasivasCsv',
+    'configurations',
+    'menues',
+    'xml',
+    'session',
+    'logs',
+    'debug',
+    '.svn',
+    #'SQL_SRC',
+    'images',
+    'tem1',
+    'fusionCharts']
